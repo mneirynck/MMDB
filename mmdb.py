91e1f4596deeabceb4e9267e5c04ebef547f6e14
Matthew <matthew.neirynck@outlook.com>
2018-10-09 00:05:29 +0200

    tmdb api connect

--- mmdb.py
+++ mmdb.py
@@ -1,15 +1,15 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from oauth2client import file, client, tools
+import tmdbsimple as tmdb
 
 scope = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets']
 sa_file = '/home/matthew/Secrets/mmdb_secret.json'
+tmdb.API_KEY = '2b9e09520a4454a1e2922fb9dde72137'
 
 def main():
-    #store = file.Storage('token.json')
-    #creds = store.get()
     creds = service_account.Credentials.from_service_account_file(sa_file, scopes=scope)
-
+    tmdb_search = tmdb.Search()
     #if not creds or cred.invalid:
     #    flow = client.flow_from_clientsecrets('client_secret.json', scope)
     #    creds = tools.run_flow(flow, store)
@@ -24,9 +24,11 @@ def main():
     if not values:
         print('No data found')
     else:
-        print('Movie, Description')
+        print(values)
         for row in values:
-            print('%s, %s' % (row[0], row[3]))
+            response = tmdb_search.movie(query=row[0])
+            print(response)
+            #print('%s, %s' % (row[0], row[3]))
 
 if __name__ == '__main__':
     main()
