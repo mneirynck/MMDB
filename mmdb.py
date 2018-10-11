7b2cad6fa7c65ff56e9e5570683e6cd42651dbbd
Matthew <matthew.neirynck@outlook.com>
2018-10-12 00:11:38 +0200

    updated with tmdb search

--- mmdb.py
+++ mmdb.py
@@ -1,34 +1,44 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from oauth2client import file, client, tools
+from pathlib import Path
+from operator import itemgetter
 import tmdbsimple as tmdb
 
 scope = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets']
-sa_file = '/home/matthew/Secrets/mmdb_secret.json'
-tmdb.API_KEY = '2b9e09520a4454a1e2922fb9dde72137'
+sa_file = '../../Secrets/mmdb_secret.json'
 
 def main():
     creds = service_account.Credentials.from_service_account_file(sa_file, scopes=scope)
-    tmdb_search = tmdb.Search()
-    #if not creds or cred.invalid:
-    #    flow = client.flow_from_clientsecrets('client_secret.json', scope)
-    #    creds = tools.run_flow(flow, store)
+
+    with open('../../Secrets/tmdb_secret') as f:
+        tmdb_api_key = [ line.strip( ) for line in list(f) ]
+
     service = build('sheets', 'V4', credentials=creds)
 
     spreadsheet = '1ege7bvaIgUs7Y4OETbtdiQ_ISgwtGzH9zlZeBKYPXY8'
-    range = 'A2:D'
+    range = 'A2:F'
     result = service.spreadsheets().values().get(spreadsheetId=spreadsheet,range=range).execute()
-
     values = result.get('values', [])
+
+    tmdb.API_KEY = tmdb_api_key
+
+    tmdb_search = tmdb.Search()
 
     if not values:
         print('No data found')
     else:
-        print(values)
         for row in values:
-            response = tmdb_search.movie(query=row[0])
-            print(response)
-            #print('%s, %s' % (row[0], row[3]))
+            request = tmdb_search.movie(query=row[0])
+            result = tmdb_search.results[0]
+            lijst = itemgetter('title', 'id', 'vote_average', 'poster_path', 'overview', 'genre_ids')(result)
+            #print(lijst)
+            movie_title = result.get('title')
+            movie_id = result.get('id')
+            value_input_option = 'RAW'
+            value_body = [movie_title, movie_id]
+            push = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheet,body=lijst)
+            update_sheet = push.execute()
 
 if __name__ == '__main__':
     main()
